fdf084b8fea6f9354b0fc84c33cc690504f0d7af
Andrew Comminos <andrewcomminos@gmail.com>
2012-06-30 19:20:31 -0700

    Work on request API.

--- src/com/acomminos/morlunk/MorlunkRequest.java
+++ src/com/acomminos/morlunk/MorlunkRequest.java
@@ -1,12 +1,15 @@
 package com.acomminos.morlunk;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
 
+import org.apache.http.HttpRequest;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -26,74 +29,124 @@ import org.apache.http.message.BasicNameValuePair;
  */
 public class MorlunkRequest {
 	
-	String url;
-	Map<String, String> arguments;
+	interface MorlunkRequestListener {
+		
+		public void onRequestSuccess(String response);
+		
+		public void onRequestFailure(MorlunkRequestError error);
+		
+	}
 	
-	public MorlunkRequest(String url, Map<String, String> arguments) {
+	public enum MorlunkRequestError {
+		ERROR_NOT_AUTHENTICATED,
+		ERROR_INVALID_REQUEST,
+		ERROR_GENERIC
+	}
+	
+	public enum MorlunkRequestType {
+		REQUEST_POST,
+		REQUEST_GET
+	}
+	
+	private String url;
+	private Map<String, String> arguments = new HashMap<String, String>();
+	private MorlunkRequestType requestType;
+	private MorlunkRequestListener listener;
+	
+	public MorlunkRequest(String url) {
 		this.url = url;
-		this.arguments = arguments;
 	}
 	
 	/**
-	 * Gets the JSON string retrieved when querying
-	 * 
-	 * @return
-	 * @throws IOException
-	 * @throws ClientProtocolException
+	 * Executes the request with either a GET or POST request to the specified URL, depending on the RequestType property.
+	 * @throws IOException 
+	 * @throws ClientProtocolException 
 	 */
-	public String fetchJSON() throws ClientProtocolException, IOException {
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpUriRequest request = null;
-		
-		if(getRequestMethod() == DucatRequestMethod.GET) {
-			String loginUrl = makeGetURL(); // Get the URL that we'll ask.
-			request = new HttpGet(loginUrl);
-		} else if(getRequestMethod() == DucatRequestMethod.POST) {
-			// NOTE: IN ORDER FOR POST TO WORK, WE NEED TO USE @csrf_exempt IN THE VIEW WE WANT TO CALL.
-			request = new HttpPost(getRequestUrl());
-			HttpPost postRequest = (HttpPost) request;
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(arguments.size());
-			for(Entry<String, String> entry : arguments.entrySet()) {
-				nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-			}
-			postRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	public void execute() throws ClientProtocolException, IOException {
+		HttpClient client = new DefaultHttpClient();
+		HttpRequest request = null;
+		switch (requestType) {
+		case REQUEST_GET:
+			request = makePostRequest();
+			break;
+		case REQUEST_POST:
+			request = makeGetRequest();
+			break;
 		}
-		// Execute and handle
-		ResponseHandler<String> handler = new BasicResponseHandler();
-		String json = httpClient.execute(request, handler);
-
-		httpClient.getConnectionManager().shutdown();
-
-		return json;
+		client.execute((HttpUriRequest) request);
+	}
+	
+	public HttpPost makePostRequest() throws UnsupportedEncodingException {
+		HttpPost postRequest = new HttpPost(getUrl());
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		for(Entry<String, String> entry : arguments.entrySet()) {
+			nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+		}
+		postRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		
+		return postRequest;
+	}
+	
+	public HttpGet makeGetRequest() {
+		HttpGet getRequest = new HttpGet(getUrl());
+		getRequest.
 	}
 	
 	/**
-	 * Creates a queryable URL for the values in hashmap arguments with the URL requestUrl.
-	 * @return A queryable URL.
+	 * Adds a new argument to be submitted via a POST or GET request.
+	 * @param key
+	 * @param value
 	 */
-	private String makeGetURL() {
-		String url = this.url;
-		
-		int index = 0;
-		// Append keys and values to URL.
-		for (Map.Entry<String, String> entry : this.arguments.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			
-			value = value.replaceAll(" ", "+"); // Remove spaces and replace with pluses
-			
-			// Add a question mark or ampersand depending on the index
-			url += (index == 0) ? "?" : "&";
-			
-			url	+= key
-				+ "="
-				+ value;
-			index++;
-		}
-		
-		System.out.println(url);
-		
+	public void addArgument(String key, String value) {
+		this.arguments.put(key, value);
+	}
+	
+	public void removeArgument(String key) {
+		this.arguments.remove(key);
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
 		return url;
 	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * @return the requestType
+	 */
+	public MorlunkRequestType getRequestType() {
+		return requestType;
+	}
+
+	/**
+	 * @param requestType the requestType to set
+	 */
+	public void setRequestType(MorlunkRequestType requestType) {
+		this.requestType = requestType;
+	}
+
+	/**
+	 * @return the listener
+	 */
+	public MorlunkRequestListener getListener() {
+		return listener;
+	}
+
+	/**
+	 * @param listener the listener to set
+	 */
+	public void setListener(MorlunkRequestListener listener) {
+		this.listener = listener;
+	}
+	
+	
 	
 }
